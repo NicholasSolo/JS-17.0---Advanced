f3c8ac63fb0b4e7d5b3f71d960dd711e3e30c7f2
NicholasSolo <ohispilledmybeer@gmail.com>
2021-02-02 13:48:43 +0300

    fix: script fixes according to comments | main.js file

--- script/main.js
+++ script/main.js
@@ -1,48 +1,48 @@
 "use strict";
 
 const lang = prompt("Выберите язык отображения:", "ru");
+const russian =
+  "Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье";
+const english =
+  "Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday";
 
 // a - через if
 
 if (lang === "ru") {
-  console.log(
-    "Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье"
-  );
+  console.log(russian);
 } else if (lang === "en") {
-  console.log("Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday");
+  console.log(english);
+} else {
+  console.log("Ошибка, повторите ввод");
 }
 
 // b - через switch-case
 
 switch (lang) {
   case "ru":
-    console.log(
-      "Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье"
-    );
+    console.log(russian);
     break;
   case "en":
-    console.log(
-      "Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday"
-    );
+    console.log(english);
     break;
+  default:
+    console.log("Ошибка, повторите ввод");
 }
 
 // c - через многомерный массив
 
-const week = [
-  ["Понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"],
-  ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
-];
+const week = {
+  ru: [russian],
+  eng: [english],
+};
 
-const output = (lang === 'ru') ? week[0].join(', ') : (lang === 'en') ? week[1].join(', ') : 'Ошибка, повторите ввод';
+console.log("через многомерный массив:", week[lang].join(""));
 
-console.log(output);
-
- 
 // Вторая задача
 
-const namePerson = prompt('Ты кто?', '');
+const namePerson = prompt("Ты кто?", "");
 
-(namePerson === 'Артём' || namePerson === 'Артем') ? console.log('Привет, директор') : (namePerson === 'Максим') ? console.log('Привет, преподаватель') : ( namePerson === null || namePerson === '' ) ? console.log('Ошибка, повторите ввод') : console.log(`Привет, ${namePerson}`);
-
-
+(namePerson === "Артём" || namePerson === "Артем") ? console.log("Привет, директор") : 
+(namePerson === "Максим") ? console.log("Привет, преподаватель") : 
+(namePerson === null || namePerson === "") ? console.log("Ошибка, повторите ввод") : 
+                                             console.log(`Привет, ${namePerson}`);
