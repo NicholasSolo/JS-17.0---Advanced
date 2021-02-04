a97c901ba0150aa0634ca21f3b6ea01f6449b465
NicholasSolo <ohispilledmybeer@gmail.com>
2021-02-04 15:12:37 +0300

    feat: added scripts for lesson05

--- script/main.js
+++ script/main.js
@@ -1,23 +1,62 @@
 "use strict";
 
-const string = '         Если в процессе чтения возникает ошибка, они возвращают полученное до ошибки число        ';
+let arr = [
+  "2343434",
+  "3434231",
+  "4444343424",
+  "10009878",
+  "5663422424",
+  "43331345",
+  "22220099899",
+];
 
-function stringTransform(parameter) {
-  if (isFinite(parameter)) {
-    alert("Ахтунг! Неверный тип данных");
-    return;
-  }
-  
-  let trimmedString = parameter.trim();
+function filterArr(arr) {
+  let result = arr.filter((item) => {
+    if (item[0] === "2" || item[0] === "4") {
+      return item;
+    }
+  });
 
-  if (trimmedString.length > 30) {
-    let result = trimmedString.slice(0, 30) + "...";
-    return result;
-  } else {
-    return trimmedString;
+  for (let i = 0; i < result.length; i++) {
+    console.log(result[i]);
   }
 }
 
-console.log(stringTransform(string));
+filterArr(arr);
 
+//вывод простого числа
 
+newIteration: for (let i = 1; i <= 100; i++) {
+  if (i === 1) {
+    continue newIteration;
+  }
+  for (let j = 2; j < i; j++) {
+    if (i % j === 0) {
+      continue newIteration;
+    }
+  }
+  console.log(i + " -" + " Делители этого числа 1 и " + i);
+}
+
+// function isNumber(number) {
+//   return !isNaN(parseFloat(number)) && isFinite(number);
+// }
+
+// function isSimple(number) {
+//   if (isNumber(number)) {
+//   }
+// }
+
+// console.log(isSimple());
+
+let a = 50;
+
+function funcMath() {
+  let a = 10;
+  return function () {
+    console.log(a * a);
+  };
+}
+
+const mathPow = funcMath();
+mathPow();
