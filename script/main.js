5e07a0f562f151c500ef56a206f5ee613a8320a5
NicholasSolo <ohispilledmybeer@gmail.com>
2021-02-14 16:59:30 +0300

    feat: added task for lesson7 advanced | main.js.file

--- script/main.js
+++ script/main.js
@@ -1,41 +1,29 @@
 "use strict";
 
-const textBox = document.querySelector(".box");	
+const textBox = document.querySelector(".box");
 
-const week = [	
-  "Понедельник",	
-  "Вторник",	
-  "Среда",	
-  "Четверг",	
-  "Пятница",	
-  "Суббота",	
-  "Воскресенье",	
-];	
+const week = [
+  "Понедельник",
+  "Вторник",
+  "Среда",
+  "Четверг",
+  "Пятница",
+  "Суббота",
+  "Воскресенье",
+];
 
-const showWeek = function (arr) {	
-  for (let i = 0; i < arr.length; i++) {	
-    textBox.innerHTML += arr[i] + "<br/>";	
+let now = new Date();
 
-    if (arr[i] == "Воскресенье") {	
-      textBox.style.fontFamily = "italic";	
-    }	
-  }	
-};	
+week.forEach((item, index, arr) => {
+  let container = document.createElement("p");
+  textBox.append(container);
+  container.innerHTML = item;
+  if (item === "Суббота" || item === "Воскресенье") {
+    container.style.fontStyle = "italic";
+  }
+  if (item.toLowerCase() === now.toLocaleString('ru', {weekday:'long'}).toLowerCase()) {
+    container.style.fontWeight = "bold";
+  }
+});
 
-showWeek(week);	
-
-var today = new Date();	
-var weekday = today.getUTCDay();	
-
-// const showWeek = function (arr) {	
-//   for (let prop of arr) {	
-//     textBox.innerHTML += prop + '<br/>';	
-
-//     if (prop == 'Суббота' ) {	
-//       textBox.style.fontStyle = 'italic';	
-//       prop.pop();	
-//     }	
-//   }	
-
-// };
 
