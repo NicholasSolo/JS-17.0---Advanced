f5807eea6ae39f16dce98bf27e81f3fe20d20887
NicholasSolo <ohispilledmybeer@gmail.com>
2021-03-06 18:34:17 +0300

    fix: word decline func fixing | main.js file

--- script/main.js
+++ script/main.js
@@ -8,32 +8,9 @@ const time1 = () => {
   const day = now.toLocaleString("ru", { weekday: "long" });
   const date = now.getDate();
 
-  const rightWord = (arg) => {
-    let output = '';
-    if (arg[1] === ' час') {
-      if (arg[0] === 0 || (arg[0] >= 5 && arg[0] <= 20)) {
-        output = arg[0] + (arg[1] + 'ов');
-        return output;
-      } else if (arg[0] === 21 || arg[0] === 1) {
-        output = arg[0] + arg[1];
-        return output;
-      } else {
-        output = arg[0] + (arg[1] + 'а');
-        return output;
-      }
-    } else {
-      if (arg[0] == 1 || arg[0] == 21 || arg[0] == 31 || arg[0] == 41 || arg[0] == 51){
-        output = arg[0] + (arg[1]+'а');
-        return output;
-      } else if (arg[0] >= 2 && arg[0] <= 4 || arg[0] >= 22 && arg[0] <= 24 || arg[0] >= 32 && arg[0] <= 34 || arg[0] >= 42 && arg[0] <= 44 || arg[0] >= 52 && arg[0] <= 54 ) {
-        output = arg[0] + (arg[1]+'ы');
-        return output;
-      } else {
-        output = arg[0] + arg[1];
-        return output;
-      } 
-    }
-   }
+  const declOfNum = (number, words) => {  
+    return words[(number % 100 > 4 && number % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(number % 10 < 5) ? number % 10 : 5]];
+  }
 
   let month = now.toLocaleString("ru", { month: "long" });
   if (month === "март" || month === "август") {
@@ -42,10 +19,9 @@ const time1 = () => {
     month = month.slice(0, month.length - 1) + "я";
   }
   const year = now.getFullYear();
-  const hour = rightWord([now.getHours(), ' час']);
-
-  const minutes = rightWord([now.getMinutes(), ' минут']);
-  const seconds = rightWord([now.getSeconds(), ' секунд']); 
+  const hour = now.getHours() + declOfNum(now.getHours(), [' час', ' часа', ' часов']);
+  const minutes = now.getMinutes() + declOfNum(now.getMinutes(), [' минута', ' минуты', ' минут']);
+  const seconds = now.getSeconds() + declOfNum(now.getSeconds(), [' секунда', ' секунды', ' секунд'])
 
   clock.textContent = `Сегодня ${day[0].toUpperCase() + day.slice(1)}, ${date} ${month} ${year} года, ${hour} ${minutes} ${seconds}`;
 }
@@ -79,5 +55,31 @@ setInterval(() => {
 }, 1000);
 
 
+// комент для себя
 
-
+// const rightWord = (arg) => {
+//   let output = '';
+//   if (arg[1] === ' час') {
+//     if (arg[0] === 0 || (arg[0] >= 5 && arg[0] <= 20)) {
+//       output = arg[0] + (arg[1] + 'ов');
+//       return output;
+//     } else if (arg[0] === 21 || arg[0] === 1) {
+//       output = arg[0] + arg[1];
+//       return output;
+//     } else {
+//       output = arg[0] + (arg[1] + 'а');
+//       return output;
+//     }
+//   } else {
+//     if (arg[0] == 1 || arg[0] == 21 || arg[0] == 31 || arg[0] == 41 || arg[0] == 51){
+//       output = arg[0] + (arg[1]+'а');
+//       return output;
+//     } else if (arg[0] >= 2 && arg[0] <= 4 || arg[0] >= 22 && arg[0] <= 24 || arg[0] >= 32 && arg[0] <= 34 || arg[0] >= 42 && arg[0] <= 44 || arg[0] >= 52 && arg[0] <= 54 ) {
+//       output = arg[0] + (arg[1]+'ы');
+//       return output;
+//     } else {
+//       output = arg[0] + arg[1];
+//       return output;
+//     } 
+//   }
+//  }
