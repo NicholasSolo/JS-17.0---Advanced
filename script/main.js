0fe267168a4108f7dff4169457130e8edbe62b66
NicholasSolo <ohispilledmybeer@gmail.com>
2021-03-12 00:12:20 +0300

    feat: added bonus task | main.js file

--- script/main.js
+++ script/main.js
@@ -1,57 +1,115 @@
 "use strict";
 
-class DomElement {
-  constructor(params) {
-    this.selector = params.selector;
-    this.height = params.height;
-    this.width = params.width;
-    this.bg = params.bg;
-    this.fontSize = params.fontSize;
-    this.position = params.position;
+class UserList {
+  constructor(register, userList, userName) {
+    this.register = document.getElementById(register);
+    this.userList = document.getElementById(userList);
+    this.userName = document.getElementById(userName);
+    this.now = new Date();
+    this.users = [];
   }
-  generateElem() {
-    let newElem;
-    if (this.selector[0] === ".") {
-      newElem = document.createElement("div");
-      newElem.classList.add(this.selector.slice(1));
-    } else if (this.selector[0] === "#") {
-      newElem = document.createElement("p");
-      newElem.id = this.selector.slice(1);
+
+  renderList() {
+    this.userList.textContent = "";
+    this.users.forEach((item) => {
+      this.createItem(item);
+    });
+    this.addToStorage();
+  }
+
+  createItem(item) {
+    let li = document.createElement("li");
+    li.key = item.key;
+    li.classList.add("userItem");
+    li.innerHTML = `Имя: ${item.firstName}, фамилия: ${item.lastName}, зарегистрирован: ${item.registered} 
+    <button class="delete">Удалить пользователя</button>`;
+    this.userList.append(li);
+  }
+
+  addToStorage() {
+    localStorage.setItem("users", JSON.stringify(this.users));
+  }
+
+  getFromStorage() {
+    let reverse = JSON.parse(localStorage.getItem("users"));
+    if (reverse !== null) {
+      reverse.forEach((item) => {
+        this.users.push(item);
+      });
     }
-    newElem.style.cssText = `height: ${this.height};  width: ${this.width};  background-color: ${this.bg};  font-size: ${this.fontSize}; position: ${this.position}`;
-    newElem.style.color = "red";
-    document.querySelector("body").append(newElem);
+  }
+
+  addUser() {
+    const now = new Date();
+    const name = prompt("Enter Name", "Вася Тёркин");
+    const login = prompt("Enter Login", "логин");
+    const password = prompt("Enter Password", "пароль");
+    const localeOptions = {
+      day: "2-digit",
+      year: "numeric",
+      month: "long",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+    };
+    this.users.push({
+      firstName: name.slice(0, name.indexOf(" ")),
+      lastName: name.slice(name.indexOf(" ")),
+      login: login,
+      password: password,
+      registered: now.toLocaleString("ru", localeOptions),
+      key: this.generateKey(),
+    });
+    this.renderList();
+  }
+
+  deleteUser(users, event) {
+    const target = event.target.closest("li");
+    users.forEach((user, index, arr) => {
+      if (user.key === target.key) arr.splice(index, 1);
+    });
+  }
+
+  loginUser(users) {
+    if (this.users.length === 0) return;
+    const login = prompt("Enter Login", "");
+    const password = prompt("Enter Password", "");
+
+    const user = users.find(
+      (item) => item.login === login && item.password === password
+    );
+    if (user === undefined) {
+      alert("User not found");
+    } else {
+      this.userName.textContent = user.firstName + "!";
+    }
+  }
+
+  eventHandler() {
+    this.register.addEventListener("click", this.addUser.bind(this));
+
+    document.addEventListener("click", (event) => {
+      const target = event.target;
+      if (target.matches(".delete")) {
+        this.deleteUser(this.users, event);
+        this.renderList();
+      } else if (target.matches("#login")) {
+        this.loginUser(this.users);
+      }
+    });
+  }
+
+  generateKey() {
+    return (
+      Math.random().toString(32).substring(2, 9) + (+new Date()).toString(32)
+    );
+  }
+
+  init() {
+    this.eventHandler();
+    this.getFromStorage();
+    this.renderList();
   }
 }
-
-
-document.addEventListener("DOMContentLoaded", () => {
-  const test = new DomElement({
-    selector: ".block",
-    height: "100px",
-    width: "100px",
-    bg: "green",
-    position: "absolute",
-  });
-  test.generateElem();
-});
-
-document.querySelector("body").addEventListener("keydown", (event) => {
-    move(event);
-});
-
-function move(event) {
-const block = document.querySelector(".block");
-
-  if (event.code === "ArrowUp") {
-    block.style.top = "-10px";
-  } else if (event.code === "ArrowDown") {
-    block.style.bottom = "-10px";
-  } else if (event.code === "ArrowLeft") {
-    block.style.left = "-10px";
-  } else if (event.code === "ArrowRight") {
-    block.style.right = "-10px";
-  } else {
-    alert("use arrows");
-  }
-}
+let uList = new UserList("registerUser", "list", "username");
+uList.init();
