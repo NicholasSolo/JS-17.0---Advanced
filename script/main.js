b71d64744dfb2305caf41646a49c30786949ffe5
NicholasSolo <ohispilledmybeer@gmail.com>
2021-03-19 23:08:17 +0300

    feat: added delayed input feature | main.js file

--- script/main.js
+++ script/main.js
@@ -1,58 +1,19 @@
 "use strict";
 
-class DomElement {
-  constructor(params) {
-    this.selector = params.selector;
-    this.height = params.height;
-    this.width = params.width;
-    this.bg = params.bg;
-    this.fontSize = params.fontSize;
-    this.position = params.position;
-  }
-  generateElem() {
-    let newElem;
-    if (this.selector[0] === ".") {
-      newElem = document.createElement("div");
-      newElem.classList.add(this.selector.slice(1));
-    } else if (this.selector[0] === "#") {
-      newElem = document.createElement("p");
-      newElem.id = this.selector.slice(1);
+const input = document.getElementById("input");
+const output = document.getElementById("output");
+
+function debounce(func, time) {
+  return function (args) {
+    let previousCall = this.lastCall;
+    this.lastCall = Date.now();
+    if (previousCall && this.lastCall - previousCall <= time) {
+      clearTimeout(this.lastCallTimer);
     }
-    newElem.style.cssText = `height: ${this.height};  width: ${this.width};  background-color: ${this.bg};  font-size: ${this.fontSize}; position: ${this.position}`;
-    document.querySelector("body").append(newElem);
-  }
+    this.lastCallTimer = setTimeout(() => func(args), time);
+  };
 }
-
-
-document.addEventListener("DOMContentLoaded", () => {
-  const test = new DomElement({
-    selector: ".block",
-    height: "100px",
-    width: "100px",
-    bg: "green",
-    position: "absolute",
-  });
-  test.generateElem();
-});
-
-document.addEventListener("keydown", (event) => {
-const block = document.querySelector(".block");
-
-  let domStyle = getComputedStyle(block);
-  let top = parseInt(domStyle.top);
-  let left = parseInt(domStyle.left);
-  switch(event.key){
-    case "ArrowUp":
-      block.style.top = top - 10 + "px";
-      break;
-    case "ArrowDown":
-      block.style.top = top + 10 + "px";
-      break;
-    case "ArrowRight":
-      block.style.left = left + 10 + "px";
-      break;
-    case "ArrowLeft":
-      block.style.left = left - 10 + "px";
-      break;
-  }
-});
+const textDelay = () => {
+  output.textContent = input.value;
+};
+input.addEventListener("input", debounce(textDelay, 500));
